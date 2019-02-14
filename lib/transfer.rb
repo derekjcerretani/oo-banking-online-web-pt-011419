1b2724fc58e2ca5e91562ed20e6611c55d65c775
Derek Cerretani <derekcerretani@gmail.com>
2019-02-14 19:09:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Transfer
 
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_accessor  :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
     @sender = sender
